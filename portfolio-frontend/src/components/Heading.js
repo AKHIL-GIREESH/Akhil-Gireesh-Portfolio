998aac5d813295c5b5480302f0d47f38bc9254e5
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-07-23 13:43:12 +0530

    Header

--- portfolio-frontend/src/components/Heading.js
+++ portfolio-frontend/src/components/Heading.js
@@ -1,8 +1,12 @@
+import "./Heading.css"
+
 const Heading = () => {
     return(
-        <>
-            Heading
-        </>
+        <div className="HeaderContainer">
+            <div className="HeaderDiv1">Div1</div>
+            <div className="HeaderDiv2">Div2</div>
+            <div className="HeaderDiv3">Div3</div>
+        </div>
     )
 }
 
