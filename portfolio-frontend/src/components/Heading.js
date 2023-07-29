234992eb01ac313264701e6b75988b2e77570063
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-07-29 07:42:41 +0530

    Header

--- portfolio-frontend/src/components/Heading.js
+++ portfolio-frontend/src/components/Heading.js
@@ -3,9 +3,10 @@ import "./Heading.css"
 const Heading = () => {
     return(
         <div className="HeaderContainer">
-            <div className="HeaderDiv1">Div1</div>
+            {/* <div className="HeaderDiv1">Div1</div>
             <div className="HeaderDiv2">Div2</div>
-            <div className="HeaderDiv3">Div3</div>
+            <div className="HeaderDiv3">Div3</div> */}
+            <h1>My Name</h1>
         </div>
     )
 }
