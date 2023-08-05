70c8c04ea3b50c067592e663f22952d0501bf558
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-08-05 23:26:42 +0530

    Canvas

--- portfolio-frontend/src/components/Heading.js
+++ portfolio-frontend/src/components/Heading.js
@@ -2,12 +2,12 @@ import "./Heading.css"
 
 const Heading = () => {
     return(
-        <div className="HeaderContainer">
+        <canvas className="HeaderContainer">
             {/* <div className="HeaderDiv1">Div1</div>
             <div className="HeaderDiv2">Div2</div>
             <div className="HeaderDiv3">Div3</div> */}
             <h1>My Name</h1>
-        </div>
+        </canvas>
     )
 }
 
