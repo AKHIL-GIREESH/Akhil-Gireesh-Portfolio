1b4dc47ab3a24a5225fa402aa3847ff99c5c848d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-08-09 23:42:31 +0530

    Canvas

--- portfolio-frontend/src/components/Heading.js
+++ portfolio-frontend/src/components/Heading.js
@@ -6,6 +6,7 @@ const Heading = () => {
             {/* <div className="HeaderDiv1">Div1</div>
             <div className="HeaderDiv2">Div2</div>
             <div className="HeaderDiv3">Div3</div> */}
+            <p>Hi, I'm</p>
             <h1>My Name</h1>
         </canvas>
     )
