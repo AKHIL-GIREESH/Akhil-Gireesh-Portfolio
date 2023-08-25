1059b5b1ec58b76ce445b74538528c4c2573d90e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-08-25 23:49:54 +0530

    htmlCanvas

--- testCanvas.js
+++ testCanvas.js
@@ -1,3 +1,27 @@
 const canvas = document.querySelector("canvas")
 
 canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const c = canvas.getContext("2d")
+
+c.fillRect(50,50,100,100)
+c.fillRect(100,100,100,100)
+
+c.beginPath()
+
+c.moveTo(50,50)
+c.lineTo(100,100)
+c.strokeStyle = "#fff"
+c.stroke()
+
+c.closePath()
+
+c.moveTo(150,50)
+c.lineTo(200,100)
+//c.strokeStyle = "#000"
+c.stroke()
+
+c.moveTo(50,150)
+c.lineTo(100,200)
+c.stroke()
