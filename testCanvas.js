ab9aec747179d32eaff054efe3257f42ebc1045a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-08-27 21:34:56 +0530

    htmlCanvas

--- testCanvas.js
+++ testCanvas.js
@@ -5,43 +5,63 @@ canvas.height = window.innerHeight
 
 const c = canvas.getContext("2d")
 
-c.fillRect(50,50,100,100)
-c.fillRect(100,100,100,100)
+// c.fillStyle = `rgba(${Math.random()*255},${Math.random()*255},${Math.random()*255},1)`
+// c.fillRect(50,50,100,100)
+// c.fillRect(100,100,100,100)
 
-c.beginPath()
+// c.beginPath()
 
-c.moveTo(50,50)
-c.lineTo(100,100)
-c.strokeStyle = "#fff"
-c.stroke()
+// c.moveTo(50,50)
+// c.lineTo(100,100)
+// c.strokeStyle = "#fff"
+// c.stroke()
 
-c.moveTo(100,100)
-c.lineTo(200,100)
-c.stroke()
+// c.moveTo(100,100)
+// c.lineTo(200,100)
+// c.stroke()
 
-c.moveTo(100,100)
-c.lineTo(100,200)
-c.stroke()
+// c.moveTo(100,100)
+// c.lineTo(100,200)
+// c.stroke()
 
-c.moveTo(50,150)
-c.lineTo(150,150)
-c.stroke()
+// c.moveTo(50,150)
+// c.lineTo(150,150)
+// c.stroke()
 
-c.moveTo(150,50)
-c.lineTo(150,150)
-c.stroke()
+// c.moveTo(150,50)
+// c.lineTo(150,150)
+// c.stroke()
 
-c.moveTo(150,150)
-c.lineTo(200,200)
-c.stroke()
+// c.moveTo(150,150)
+// c.lineTo(200,200)
+// c.stroke()
 
-c.beginPath()
-c.moveTo(150,50)
-c.lineTo(200,100)
-c.strokeStyle = "#000"
-c.stroke()
+// c.beginPath()
+// c.moveTo(150,50)
+// c.lineTo(200,100)
+// c.strokeStyle = "#000"
+// c.stroke()
 
-c.moveTo(50,150)
-c.lineTo(100,200)
-//c.strokeStyle = "#000"
-c.stroke()
+// c.moveTo(50,150)
+// c.lineTo(100,200)
+// //c.strokeStyle = "#000"
+// c.stroke()
+
+let x = 200
+let flag = false
+function animate(){
+    c.clearRect(0,0,innerWidth,innerHeight)
+    requestAnimationFrame(animate)
+    c.beginPath()
+    c.arc(x,200,50,0,360,false);
+    c.stroke();
+    if(x+50>=innerWidth){
+        flag = true
+    }
+    if(x-50<=0){
+        flag = false
+    }
+    flag?x-=1:x+=1
+}
+
+//animate()
