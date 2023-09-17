46db2e96d9e9e543a4a6d6d905a5cae1d08a7fd5
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-09-17 12:59:15 +0530

    ui

--- testLanding.js
+++ testLanding.js
@@ -1,4 +1,75 @@
-//console.log("Hello World")
+let myDocument = document.documentElement;
+//let btn = document.getElementById("btn");
+document.addEventListener("dblclick", ()=>{
+    if(!(document.fullscreenElement)){
+        if (myDocument.requestFullscreen) {
+            myDocument.requestFullscreen();
+        }
+        else if (myDocument.msRequestFullscreen) {
+            myDocument.msRequestFullscreen();
+        } 
+        else if (myDocument.mozRequestFullScreen) {
+            myDocument.mozRequestFullScreen();
+        }
+        else if(myDocument.webkitRequestFullscreen) {
+            myDocument.webkitRequestFullscreen();
+        }
+        //btn.textContent = "Exit Fullscreen";
+    }
+    else{
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if(document.msexitFullscreen) {
+            document.msexitFullscreen();
+        }
+        else if(document.mozexitFullscreen) {
+            document.mozexitFullscreen();
+        }
+        else if(document.webkitexitFullscreen) {
+            document.webkitexitFullscreen();
+        }
+        //btn.textContent = "Go Fullscreen";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
 
     const canvas = document.querySelector("canvas")
     const ipadCursor = document.querySelector(".ipCursor")
